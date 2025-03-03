20d45f13abd5219bf9bf0290f314821eb211c385
Alishan Shaikh <rwbn1.alishan.as@gmail.com>
2025-03-03 12:03:15 +0530

    do_while loops, sum, factorial,  no of digit

---
+++ 07_cpp_loop/do_while_loop.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i = 1;
+
+    
+    do{
+        
+        i++;
+        cout << i << endl;
+
+        // 2 // 3 //4 // 5
+    }while(i < 5); // true// true// true // false
+
+
+    // 2
+    // 3
+    // 4
+    // 5
+
+    return 0;
+}
---
+++ 07_cpp_loop/sum_digit.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+
+    int i = 12854; 
+
+    int num_digit = 0;
+
+    while(i>0)
+    {
+        i = i/10; 
+
+        num_digit++;
+    }
+    cout << num_digit <<endl;
+
+    return 0;
+}
