fee2e5e87b77ce2c6fa75baf291c95bd2fa0a9eb
Alishan Shaikh <rwbn1.alishan.as@gmail.com>
2025-04-16 11:56:55 +0530

    arr obj & static keyword

---
+++ 27_cpp_arr_obj/index.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+
+
+
+class Product {
+
+        public:
+            char name[10];
+            int price;
+            float rate;
+
+};
+
+int main(){
+
+
+    int size;
+    cout << "size: ";
+    cin >> size;
+
+        Product item[size];
+
+
+        for(int i=0; i<size; i++){
+            cout << "name: ";
+            cin >> item[i].name;
+            cout << "price: ";
+            cin >> item[i].price;
+            cout << "rate: "; 
+            cin >> item[i].rate;
+        }
+
+
+  for(int i=0; i<size; i++)
+  {
+    cout << "-----------------------------------------------" << endl;
+      cout << "| " << item[i].name << " | " << item[i].price << " | " << item[i].rate << " |" << endl;
+  }
+
+    return 0;
+}
---
+++ 27_cpp_arr_obj/scope.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+
+
+int x = 100;
+
+int main(){
+
+    int x = 20;
+
+        cout << x << endl;
+        cout << ::x << endl;
+
+    
+    return 0;
+}
